1036c742f0ce61c50362a2d934a0381cef04777f
vijay6460 <vijay.vijju499@gmail.com>
2022-02-15 19:28:45 +0530

    Basics

---
+++ objects.js
@@ -0,0 +1,45 @@
+//Object literal syntax or Object declaration
+
+// let brand={
+// car1:'bugatti', price:41,color:'red'
+// }
+
+//creating the object
+let cars={brandnews:'lamborghini',year:2020,price:41}
+
+// reading the object
+console.log(cars)
+
+// accessing the properties in the object
+// here . is the opeartor
+// console.log(cars.brand)
+// console.log(cars.year)
+// console.log(cars.price)
+
+//another way of accesing the object property
+// console.log(cars['brand']);
+// console.log(cars['price']);
+// console.log(cars['year']);
+
+//another way of accessing the object
+
+// let news='lambo'
+// console.log("brand"+news)
+
+// another way of accessing the object
+// prompt("enter the number")
+// console.log(Number(prompt("enter the letter you want to display")))
+
+// when ever we are trying to access the object which does not exist
+// then we will get undefined
+
+// adding properties to the object
+
+cars.location="mumbai";
+cars.number=58454;
+console.log(cars)
+
+
+
+
+
